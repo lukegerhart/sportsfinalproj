98caf8554358f1cec82d0c06f36f6084d337a49e
Luke Gerhart <luke_gerhart@hotmail.com>
2018-04-24 15:32:22 -0400

    start formatting report

--- onside.py
+++ onside.py
@@ -39,11 +39,14 @@ away = [x[72] for x in onsides[1:]]
 kickingteam = [x[17] for x in onsides[1:]]
 
 scorediff = [-int(x[69]) for x in onsides[1:]]
-x, y = zip(*sorted(zip(scorediff, wpa)))
-plt.scatter(x, y)
-plt.title('Score Differential vs. WPA For Kicking Team')
-plt.xlabel('Kicking Team\'s Score Margin')
-plt.ylabel('Kicking Teams\'s Win Probability Added')
+
+
+# x, y = zip(*sorted(zip(scorediff, wpa)))
+# plt.scatter(x, y)
+# plt.title('Score Differential vs. WPA For Kicking Team')
+# plt.xlabel('Kicking Team\'s Score Margin')
+# plt.ylabel('Kicking Teams\'s Win Probability Added')
+
 #State of game score pie chart
 # scorediff = [x[69] for x in onsides[1:]]
 # abs_scorediff = [x[70] for x in onsides[1:]]
@@ -61,7 +64,7 @@ plt.ylabel('Kicking Teams\'s Win Probability Added')
 # plt.title('Game Score State During Onside Kicks')
 # plt.pie(sizes, labels=labels, startangle=130, autopct='%1.1f%%', shadow=True)
 
-
+# plot for winprob vs recovery rate
 # print(winprob_pre)
 # bins = np.array([0.0, 0.01, 0.02, 0.070, 0.35, 1])
 # x = np.array(winprob_pre)
@@ -79,10 +82,27 @@ plt.ylabel('Kicking Teams\'s Win Probability Added')
 # for i in range(1,6):
 	# rates.append(binned_rates[i])
 # plt.xticks(np.arange(5), ('0.0-0.01', '0.01-0.02', '0.02-0.07', '0.07-0.35', '0.35-1'))
-# plt.title('Onside Kick Percentage By WinProb of Kicking Team Pre-kick')
+# plt.title('Onside Kick Recovery Rate By WinProb of Kicking Team Pre-kick')
 # plt.xlabel('WinProb')
-# plt.ylabel('Percentage')
+# plt.ylabel('Recovery Rate')
 # plt.plot([0,1,2,3,4], rates)
+
+# plot for winprob vs kick frequency
+# bins = np.array([0.0, 0.1, 0.2, 0.3, 0.5, 0.6, 0.7, 0.8, 0.9, 1])
+# x = np.array(winprob_pre)
+# inds = np.digitize(x, bins)
+# counts = collections.Counter(inds)
+# print(counts)
+# total = sum(counts.values())
+# rates = []
+# for i in range(1, len(counts.keys())+1):
+	# rates.append(counts[i]/total)
+# print(rates)
+# plt.xticks(np.arange(9), ('0-.1', '.1-.2','.2-.3','.3-.4','.4-.5','.5-.6','.6-.7','.7-.8','.8-.9','.9-1'))
+# plt.title('WinProb vs. Onside Attempt Frequency')
+# plt.xlabel('WinProb')
+# plt.ylabel('Percentage of Total Onside Attempts')
+# plt.plot([0,1,2,3,4,5,6,7,8], rates)
 
 # pie chart for recovery rate
 # labels = 'Recoveries', 'Failures'
